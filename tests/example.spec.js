138b9be6ccb7fa886f97056dad27f8143c7bb0d7
NodesQA <70371590+NodesQA@users.noreply.github.com>
2025-08-28 23:15:18 +0300

    first commit

---
+++ tests/example.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('UEFA Gaming basic UI checks', async ({ page }) => {
+  await page.goto('https://gaming.uefa.com/en/uefachampionsleague');
+  await page.getByRole('button', { name: '✓Accept all' }).click();
+
+  await expect(page.getByText('Unlock the full experience! Log in;')).toBeVisible();
+
+  await expect(page.getByText('Take on your friends – and')).toBeVisible();
+
+  await expect(page.getByRole('button', { name: 'Log in', exact: true })).toBeVisible();
+
+  await page.getByRole('button', { name: 'Log in', exact: true }).click();
+
+  await expect(page.locator('#username')).toBeVisible();
+});
